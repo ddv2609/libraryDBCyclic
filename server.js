df3760650e33934a82843072e8dcc94339d802d6
ddv2609 <vien26092k2@gmail.com>
2023-07-13 18:10:06 +0700

    change server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // server.js
 const jsonServer = require('json-server')
+// Import the library:
+const cors = require('cors');
+
 const server = jsonServer.create()
+// Then use it before your routes are set up:
+server.use(cors());
+
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
